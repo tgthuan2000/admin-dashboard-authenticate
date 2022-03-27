c0f39ccbfd89bd22301f0ad3ba9a2adcb8c6fa2d
tgthuan2000 <tgthuan2000@gmail.com>
2022-03-27 21:58:12 +0700

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -9,23 +9,19 @@ const router = express.Router()
 // @route POST /auth/local/login
 // @desc Authenticate with username password
 // @access Public
-router.post(
-	'/local/login',
-	passport.authenticate('local', { failureRedirect }),
-	({ user }, res) => {
-		if (user) {
-			const tokenAccess = jwt.sign({ _id: user._id }, process.env.SECRET_JWT, {
-				expiresIn: 24 * 60 * 60 * 1000,
-			})
-			res.json({ success: true, tokenAccess, user })
-		}
+router.post('/login', passport.authenticate('local', { failureRedirect }), ({ user }, res) => {
+	if (user) {
+		const tokenAccess = jwt.sign({ _id: user._id }, process.env.SECRET_JWT, {
+			expiresIn: 24 * 60 * 60 * 1000,
+		})
+		res.json({ success: true, tokenAccess, user })
 	}
-)
+})
 
 // @route GET /auth/local/re-login
 // @desc Authenticate with jwt
 // @access Public
-router.get('/local/re-login', passport.authenticate('jwt', { session: false }), ({ user }, res) => {
+router.get('/re-login', passport.authenticate('jwt', { session: false }), ({ user }, res) => {
 	if (user) {
 		res.json({ success: true, user })
 	}
