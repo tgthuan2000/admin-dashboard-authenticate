52798120b7f3fa6529361730784d441a4e088d24
tgthuan2000 <tgthuan2000@gmail.com>
2022-05-03 22:26:42 +0700

    cm

--- src/query.js
+++ src/query.js
@@ -1,23 +1,31 @@
 export const LOGIN = `
-    *[_type == "user" && username == $username && password == $password && references('${process.env.ADMIN_ROLE}')] 
+    *[_type == "user" && username == $username && password == $password && role._id != '${process.env.CUSTOMER_ROLE}'] 
     { 
         _id,
         fullName,
         email,
         phone,
         address,
-        username
+        username,
+        role-> {
+            _id,
+            name
+        }
     }
 `
 
 export const RE_LOGIN = `
-    *[_type == "user" && _id == $_id && references('${process.env.ADMIN_ROLE}')] 
+    *[_type == "user" && _id == $_id && role._id != '${process.env.CUSTOMER_ROLE}'] 
     { 
         _id, 
         fullName, 
         email, 
         phone, 
         address, 
-        username 
+        username,
+        role-> {
+            _id,
+            name
+        } 
     }
 `
