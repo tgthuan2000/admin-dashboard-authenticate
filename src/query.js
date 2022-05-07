2aee460c9674377305f241990a1b581700c9ee0e
Tran Gia Thuan <86921075+tgthuan2000@users.noreply.github.com>
2022-05-07 16:19:55 +0700

    Update query.js

--- src/query.js
+++ src/query.js
@@ -2,11 +2,14 @@ export const LOGIN = `
     *[_type == "user" && username == $username && password == $password && role._ref != '${process.env.CUSTOMER_ROLE}'] 
     { 
         _id,
+        _createdAt,
+        _updatedAt,
+        username,
         fullName,
         email,
         phone,
         address,
-        username,
+        image,
         role-> {
             _id,
             name
@@ -17,15 +20,18 @@ export const LOGIN = `
 export const RE_LOGIN = `
     *[_type == "user" && _id == $_id && role._ref != '${process.env.CUSTOMER_ROLE}'] 
     { 
-        _id, 
-        fullName, 
-        email, 
-        phone, 
-        address, 
+        _id,
+        _createdAt,
+        _updatedAt,
         username,
+        fullName,
+        email,
+        phone,
+        address,
+        image,
         role-> {
             _id,
             name
-        } 
+        }
     }
 `
